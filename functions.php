b60be0f4f7a32bf7ec72f0392cb050782868145b
robmccormack89 <robertmccormack147@gmail.com>
2021-06-30 17:41:26 +0100

    added all required plugins

--- functions.php
+++ functions.php
@@ -12,7 +12,9 @@ require_once get_template_directory() . '/inc/lib/class-tgm-plugin-activation.ph
 function dream_winners_register_required_plugins() {
 	$plugins = array(
 		
-		// This is an example of how to include a plugin bundled with a theme.
+		// This is an example of how to include a plugin bundled with a theme. Nothing is required
+    
+    // pre-packaged
     
     // acf pro
     array(
@@ -63,6 +65,191 @@ function dream_winners_register_required_plugins() {
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
     
+    // custom store notice for woo
+    array(
+      'name'               => 'Custom Store Notice for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'custom-store-notice-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/custom-store-notice-plugin-main.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // direct to checkout for woo
+    array(
+      'name'               => 'Direct to Checkout for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'direct-to-checkout-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/direct-to-checkout-plugin-main.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // orderby with cats for woo
+    array(
+      'name'               => 'Orderby with Categories for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'orderby-with-categories-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/orderby-with-categories-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // relates & upsells for woo
+    array(
+      'name'               => 'Related & Upsells for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'related-upsells-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/related-upsells-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    
+    // header search popup for woo
+    array(
+      'name'               => 'Header Search Popup for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'header-search-popup-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/header-search-popup-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // minicart popup for woo
+    array(
+      'name'               => 'Minicart Popup for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'minicart-popup-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/minicart-popup-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // site preloader
+    array(
+      'name'               => 'Site Animated Preloader by RMcC', // The plugin name.
+			'slug'               => 'site-preloader-animated-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/site-preloader-animated-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // cf7 subscribe section
+    array(
+      'name'               => 'Subscribe Section for CF7 by RMcC', // The plugin name.
+			'slug'               => 'cf7-subscribe-section-form-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/cf7-subscribe-section-form-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    
+    // ajax result count subtotal
+    array(
+      'name'               => 'Ajax Subtotal & Result Count for Woocommerce by RMcC', // The plugin name.
+			'slug'               => 'ajax-subtotal-result-count-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/ajax-subtotal-result-count-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // blog posts section
+    array(
+      'name'               => 'Blog Posts Section by RMcC', // The plugin name.
+			'slug'               => 'blog-posts-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/blog-posts-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // contact section
+    array(
+      'name'               => 'Contact Section by RMcC', // The plugin name.
+			'slug'               => 'contact-section-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/contact-section-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // featured item section
+    array(
+      'name'               => 'Featured Item Section by RMcC', // The plugin name.
+			'slug'               => 'featured-item-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/featured-item-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // featured page blocks section
+    array(
+      'name'               => 'Featured Page Blocks Section by RMcC', // The plugin name.
+			'slug'               => 'featured-page-blocks-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/featured-page-blocks-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // testimonials section
+    array(
+      'name'               => 'Testimonials Rating Section by RMcC', // The plugin name.
+			'slug'               => 'testimonials-rating-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/testimonials-rating-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    // video popup section
+    array(
+      'name'               => 'Video Popup Section by RMcC', // The plugin name.
+			'slug'               => 'video-popup-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/lib/plugins/video-popup-plugin-master.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+    
+    // wp plugin repository
+    
     // woo
 		array(
       'name' => 'WooCommerce',
@@ -79,6 +266,12 @@ function dream_winners_register_required_plugins() {
     array(
       'name' => 'Loco Translate',
 			'slug' => 'loco-translate',
+			'required' => false
+		),
+    // custom product tabs
+    array(
+      'name' => 'Custom Product Tabs for WooCommerce',
+			'slug' => 'yikes-inc-easy-custom-woocommerce-product-tabs',
 			'required' => false
 		),
     
