98c060bef3bb21088bfaa28fb980c24a8cab20a5
robmccormack89 <robertmccormack147@gmail.com>
2022-01-05 09:20:08 +0000

    page bulder template

---
+++ page-templates/page-builder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Page Builder Template
+ * Template Post Type: post, page
+ *
+ * @package Loadingdock_Theme
+ */
+
+$context = Timber::context();
+$post = Timber::query_post();
+$context['post'] = $post;
+Timber::render('page-builder-page.twig', $context);
