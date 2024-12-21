438418b6bc98ab1440aa15fd66896db02fac0831
Robert McCormack <robertmccormack147@gmail.com>
2024-12-21 17:28:54 +0000

    del

--- _dev/v1/README.md
+++
@@ -1,3 +0,0 @@
-# Rmcc Theme
-
----
--- _dev/v2/_old/src/js/index.js
+++
@@ -1,6 +0,0 @@
-import UIkit from 'uikit'; // import uikit
-import Icons from 'uikit/dist/js/uikit-icons'; // import uikit icons
-UIkit.use(Icons); // use the Icon plugin
-
-// load infinite scroll
-window.InfiniteScroll = require('infinite-scroll');
