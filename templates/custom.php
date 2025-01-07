ae60ac2b9d20e4f0b3f0b86ea01bddf3de24b71a
Robert <robertmccormack147@gmail.com>
2025-01-07 12:50:05 +0000

    format

--- templates/custom.php
+++ templates/custom.php
@@ -50,24 +50,24 @@ $context['post']->the_excerpt = $context['post']->post_excerpt ?: false; // set 
 // if not a privated post (privated posts will appear as 404s due to configs above)
 if (get_post_status($context['post']->ID) != 'private') {
 
-	// good housekeeping. we will use post.title & post.post_excerpt in actual templates
-	$context['title'] = $context['post']->title;
-	$context['description'] = $context['post']->post_excerpt ?: false;
+  // good housekeeping. we will use post.title & post.post_excerpt in actual templates
+  $context['title'] = $context['post']->title;
+  $context['description'] = $context['post']->post_excerpt ?: false;
 
-	// twig templates for singles
-	array_unshift(
-		$templates,
-		'single-' . $context['post']->ID . '.twig',
-		'single-' . $context['post']->slug . '.twig',
-		'single-' . $context['post']->post_type . '.twig',
-		'single.twig'
-	);
+  // twig templates for singles
+  array_unshift(
+    $templates,
+    'single-' . $context['post']->ID . '.twig',
+    'single-' . $context['post']->slug . '.twig',
+    'single-' . $context['post']->post_type . '.twig',
+    'single.twig'
+  );
 
-	// add the custom template/s to the start of the templates array
-	array_unshift($templates, 'custom-' . $context['post']->post_type . '.twig', 'custom.twig', );
+  // add the custom template/s to the start of the templates array
+  array_unshift($templates, 'custom-' . $context['post']->post_type . '.twig', 'custom.twig', );
 
-	// add new template for password protected singulars (does not work on static front_pages)
-	if (post_password_required($context['post'])) array_unshift($templates, 'single_protected.twig');
+  // add new template for password protected singulars (does not work on static front_pages)
+  if (post_password_required($context['post'])) array_unshift($templates, 'single_protected.twig');
 
 }
 
