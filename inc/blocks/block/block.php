7fde77f7b8d8520900bab3e67b612b43667509ad
Robert <robertmccormack147@gmail.com>
2025-03-12 16:04:06 +0000

    block with group container

--- inc/blocks/block/block.php
+++ inc/blocks/block/block.php
@@ -14,14 +14,42 @@ $context['block']['fields'] = get_fields();
 $context['block']['is_preview'] = $is_preview;
 $context['block']['controls'] = new Block($context['block']);
 $context['block']['template'] = array(
-  array('core/heading', array(
-    'level' => 2,
-    'content' => 'Primary',
-    'className' => 'rmcc-card-title',
-  )),
-  array('core/paragraph', array(
-    'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    'className' => 'rmcc-margin-remove',
-  ))
+  array(
+    'core/group',
+    array(
+      'style' => array(
+        'spacing' => array(
+          'blockGap' => '20px'
+        )
+      ),
+      'layout' => array(
+        'type' => 'default'
+      )
+    ),
+    array(
+      array(
+        'core/heading',
+        array(
+          'level' => 2,
+          'content' => 'Primary',
+          'className' => 'rmcc-card-title rmcc-margin-remove-bottom'
+        ),
+        array()
+      ),
+      array(
+        'core/paragraph',
+        array(
+          'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          'className' => 'rmcc-margin-remove-bottom'
+        ),
+        array()
+      ),
+    )
+  )
 );
+
+
+
+
+
 Theme::render('block.twig', $context);
