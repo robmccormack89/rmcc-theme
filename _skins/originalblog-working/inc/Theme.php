bb97839be387f491519e4e91384e640a75042635
Robert <robertmccormack147@gmail.com>
2025-10-14 14:25:18 +0100

    sorting

--- _skins/originalblog-working/inc/Theme.php
+++ _skins/originalblog-working/inc/Theme.php
@@ -46,6 +46,9 @@ class Theme extends Timber {
     add_action('init', array($this, 'register_navigation_menus'));
     add_action('enqueue_block_assets', array($this, 'theme_enqueue_assets'));
 
+    // filter what html tags & attrs are allowed in wp content / wp kses post
+    add_action('init', array($this, 'allowed_html_tags_attrs'), 10);
+
     // Remove tags support from posts
     if (array_key_exists('enable_post_tags', $this->configs) && $this->configs['enable_post_tags'] != true) {
       add_action('init', function () {
@@ -56,6 +59,104 @@ class Theme extends Timber {
       });
     }
 
+  }
+
+  // filter what html tags & attrs are allowed in wp content / wp kses post
+  public function allowed_html_tags_attrs() {
+    global $allowedposttags;
+    $allowed_atts = array(
+      'align' => array(),
+      'class' => array(),
+      'type' => array(),
+      'id' => array(),
+      'dir' => array(),
+      'lang' => array(),
+      'style' => array(),
+      'xml:lang' => array(),
+      'src' => array(),
+      'alt' => array(),
+      'href' => array(),
+      'rel' => array(),
+      'rev' => array(),
+      'target' => array(),
+      'novalidate' => array(),
+      'type' => array(),
+      'value' => array(),
+      'name' => array(),
+      'tabindex' => array(),
+      'action' => array(),
+      'method' => array(),
+      'for' => array(),
+      'width' => array(),
+      'height' => array(),
+      'data' => array(),
+      'title' => array(),
+      'hidden' => array(),
+      'role' => array(),
+      'aria-live' => array(),
+      'aria-atomic' => array(),
+      'data-status' => array(),
+      'data-template' => array(),
+      'aria-required' => array(),
+      'aria-invalid' => array(),
+      'aria-describedby' => array(),
+      'data-name' => array(),
+      'size' => array(),
+      'role' => array(),
+      'aria-hidden' => array(),
+      'focusable' => array(),
+      'role' => array(),
+      'viewBox' => array(),
+      'fill' => array(),
+      'd' => array(),
+      'data-nanogallery2' => array(),
+      'rmcc-slider-parallax' => array(),
+      'rmcc-accordion' => array(),
+      'rmcc-icon' => array(),
+      'rmcc-slider' => array(),
+      'rmcc-grid' => array(),
+      'rmcc-form' => array(),
+      'rmcc-modal' => array(),
+      'rmcc-toggle' => array(),
+    );
+    $allowedposttags['form'] = $allowed_atts;
+    $allowedposttags['button'] = $allowed_atts;
+    $allowedposttags['cite'] = $allowed_atts;
+    $allowedposttags['svg'] = $allowed_atts;
+    $allowedposttags['path'] = $allowed_atts;
+    $allowedposttags['label'] = $allowed_atts;
+    $allowedposttags['input'] = $allowed_atts;
+    $allowedposttags['textarea'] = $allowed_atts;
+    $allowedposttags['iframe'] = $allowed_atts;
+    $allowedposttags['script'] = $allowed_atts;
+    $allowedposttags['style'] = $allowed_atts;
+    $allowedposttags['strong'] = $allowed_atts;
+    $allowedposttags['small'] = $allowed_atts;
+    $allowedposttags['table'] = $allowed_atts;
+    $allowedposttags['span'] = $allowed_atts;
+    $allowedposttags['abbr'] = $allowed_atts;
+    $allowedposttags['code'] = $allowed_atts;
+    $allowedposttags['pre'] = $allowed_atts;
+    $allowedposttags['div'] = $allowed_atts;
+    $allowedposttags['img'] = $allowed_atts;
+    $allowedposttags['h1'] = $allowed_atts;
+    $allowedposttags['h2'] = $allowed_atts;
+    $allowedposttags['h3'] = $allowed_atts;
+    $allowedposttags['h4'] = $allowed_atts;
+    $allowedposttags['h5'] = $allowed_atts;
+    $allowedposttags['h6'] = $allowed_atts;
+    $allowedposttags['ol'] = $allowed_atts;
+    $allowedposttags['ul'] = $allowed_atts;
+    $allowedposttags['li'] = $allowed_atts;
+    $allowedposttags['em'] = $allowed_atts;
+    $allowedposttags['hr'] = $allowed_atts;
+    $allowedposttags['br'] = $allowed_atts;
+    $allowedposttags['tr'] = $allowed_atts;
+    $allowedposttags['td'] = $allowed_atts;
+    $allowedposttags['p'] = $allowed_atts;
+    $allowedposttags['a'] = $allowed_atts;
+    $allowedposttags['b'] = $allowed_atts;
+    $allowedposttags['i'] = $allowed_atts;
   }
 
   // theme supports & assets
