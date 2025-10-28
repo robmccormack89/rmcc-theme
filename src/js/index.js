3c82ec4b21e2fc9d662f82c7382b39533e90cfef
Robert <robertmccormack147@gmail.com>
2025-10-28 11:14:23 +0000

    webpack

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import UIkit from 'uikit'; // import uikit
+import Icons from 'uikit/dist/js/uikit-icons'; // import uikit icons
+UIkit.use(Icons); // use the Icon plugin
+window.RMcC = UIkit; // Make uikit available in window for inline scripts
