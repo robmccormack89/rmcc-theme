228dd6e6416cc8481aea1a4e78d2e34626cb0700
Robert <robertmccormack147@gmail.com>
2025-11-05 12:31:19 +0000

    current boilerplate

---
+++ _skins/_current-boilerplate/inc/extra/configs.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+*
+* Global theme configs to enable & disable various shit
+*
+* @package Rmcc_Theme
+*
+*/
+
+/*
+Configs &
+Settings
+*/
+
+$configs['logo_width'] = '223';
+$configs['logo_height'] = '36';
+
+/*
+Enable
+Disable
+Stuff
+*/
+
+$configs['enable_post_tags'] = true;
+$configs['enable_page_excerpts'] = true;
+$configs['enable_post_comments'] = true;
+$configs['enable_post_sharing'] = true;
+$configs['enable_post_paging'] = true;
+$configs['enable_post_author'] = true;
+
+/*
+ACF
+*/
+
+$configs['enable_acf'] = true;
+$configs['hide_acf_menus'] = false;
+
+return $configs;
